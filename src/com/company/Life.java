dd5e7d5e1ea14be78d503a198a2699beef73a6f5
EmaLou <loujia1991@gmail.com>
2013-11-21 23:51:21 +0800

    add test for keep alive when have 3 alive around

---
+++ src/com/company/Life.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Life {
+    public LifeStatus status;
+    public int aliveAround;
+
+    public int calculateAliveAround() {
+        return aliveAround;
+    }
+
+    public LifeStatus currentStatus(Enum lifeStatus, int aliveAround) {
+        if(isCanLive(lifeStatus, aliveAround))
+            return LifeStatus.alive;
+        return LifeStatus.died;
+    }
+
+    private boolean isCanLive(Enum lifeStatus, int aliveAround) {
+        return (aliveAround == 2 || aliveAround == 3) && lifeStatus == LifeStatus.alive;
+    }
+
+}
--- test/LifeTest.java
+++ test/LifeTest.java
@@ -1,3 +1,5 @@
+import com.company.Life;
+import com.company.LifeStatus;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -8,7 +10,18 @@ public class LifeTest {
     public void shouldKeepAliveWhenThereAreTwoLiveCell() throws Exception {
         Life life = new Life();
         life.status = LifeStatus.alive;
+        life.aliveAround = 2;
         int aliveNumber = life.calculateAliveAround();
         assertThat(life.currentStatus(life.status, aliveNumber), is(LifeStatus.alive));
+    }
+
+    @Test
+    public void shouldKeepAliveWhenThereAreThreeLiveCell() throws Exception {
+        Life life = new Life();
+        life.status = LifeStatus.alive;
+        life.aliveAround = 3;
+        int aliveNumber = life.calculateAliveAround();
+        assertThat(life.currentStatus(life.status, aliveNumber), is(LifeStatus.alive));
+
     }
 }
